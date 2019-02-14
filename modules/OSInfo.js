416537bdd599f94ceecf800378c0ab4799b3be25
Dagmara Bełczowska <dagmara.belczowska@wp.pl>
2019-02-14 21:38:38 +0100

    Add colors in OSinfo.js

--- modules/OSInfo.js
+++ modules/OSInfo.js
@@ -1,4 +1,5 @@
 var os = require('os');
+var colors = require('colors');
 
 function getOSinfo() {
     var type = os.type();
@@ -12,11 +13,11 @@ function getOSinfo() {
     var uptime = os.uptime();
     var userInfo = os.userInfo();
 
-    console.log('System:', type);
-    console.log('Release:', release);
-    console.log('CPU model:', cpu);
-    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
-    console.log('User name:', userInfo.username);
+    console.log('System:'.grey, type);
+    console.log('Release:'.red, release);
+    console.log('CPU model:'.blue, cpu);
+    console.log('Uptime: ~'.green, (uptime / 60).toFixed(0), 'min');
+    console.log('User name:'.yellow, userInfo.username);
     console.log('Home dir:', userInfo.homedir);
 }
 
