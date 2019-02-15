156797bbb8f59ff47d4ee227cd8824f01a9b7057
Dagmara Bełczowska <dagmara.belczowska@wp.pl>
2019-02-15 14:26:39 +0100

    Add .gitignore

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+var EventEmitter = require('events').EventEmitter;
+var OSinfo = require('./modules/OSinfo');
+
+var emitter = new EventEmitter();
+
+emitter.on('beforeCommand', function(instruction) {
+    console.log('You wrote: ' + instruction + ' trying to run command.');
+});
+
+emitter.on('afterCommand', function() {
+    console.log('Finished command');
+});
+
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
     if (input !== null) {
         var instruction = input.trim();
+        emitter.emit('beforeCommand', instruction);
         switch(instruction) {
             case '/exit':
                 process.stdout.write('Quitting app!\n');
@@ -17,8 +31,8 @@ process.stdin.on('readable', function() {
             default:
                 process.stderr.write('Wrong instruction!\n');
         };
+        emitter.emit('afterCommand');
     }
 });
 
-var OSinfo = require('./modules/OSinfo');
 
